01260742eddc7f3d76079c374d55969b18ad3fe4
vikaskumarrr9 <vikaskumarrr9@gmail.com>
2025-05-30 02:54:19 +0530

    Resume Path Changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = '/VIKAS_KUMAR_RESUME.pdf';
+    const resumeUrl = 'public/VIKAS_KUMAR_RESUME.pdf';
     window.open(resumeUrl);
   };
 
