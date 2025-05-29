290d6e254224658fa2c7a95f99811f59ee21ffc6
vikaskumarrr9 <vikaskumarrr9@gmail.com>
2025-05-30 03:14:17 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = 'vikas-portfolio/VIKAS_KUMAR_RESUME.pdf';
+    const resumeUrl = '/VIKAS_KUMAR_RESUME.pdf';
     window.open(resumeUrl);
   };
 
